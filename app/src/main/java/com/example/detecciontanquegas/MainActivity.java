73d22c39db2f043b6c537ec597926c7285db47e8
luis zamora <lzamora@espol.edu.ec>
2020-01-21 18:10:25 -0500

    cambios importantes

--- app/src/main/java/com/example/detecciontanquegas/MainActivity.java
+++ app/src/main/java/com/example/detecciontanquegas/MainActivity.java
@@ -11,6 +11,7 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,12 +20,19 @@ import android.widget.VideoView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.regex.Pattern;
 
@@ -33,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
     EditText txtUsuario, txtPasswd;
     Button btnLogin, btnRegistro;
     TextView registrobtn;
+    GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInOptions gso;
+    static final int GOOGLE_SIGN_IN = 123;
+    ImageView googleicon;
     //datos para iniciar sesion
     String email;
     String password;
@@ -55,7 +67,13 @@ public class MainActivity extends AppCompatActivity {
 
         Videothing(); // SOBRE EL VIDEO
 
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.google_app_id)).requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+
+        googleicon = (ImageView) findViewById(R.id.googleicon);
         txtUsuario = (EditText) findViewById(R.id.txtUsuario);
         txtPasswd = (EditText) findViewById(R.id.txtPasswd);
         btnLogin = (Button) findViewById(R.id.btnLogin);
@@ -67,9 +85,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
+        googleicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
+            }
+        });
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -150,6 +172,38 @@ public class MainActivity extends AppCompatActivity {
                 if (mCurrentVideoPosition != 0) {
                     mMediaPlayer.seekTo(mCurrentVideoPosition);
                     mMediaPlayer.start();
+                }
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(
+                    data);
+            try {
+                GoogleSignInAccount account =task.getResult(ApiException.class);
+                if (account != null)
+                    firebaseAuthWithGoogle(account);
+
+            } catch (ApiException e) {
+                Log.w("TAG", "Fallo el inicio de sesión con google.", e);
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(),null);
+        nAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = nAuth.getCurrentUser();
+                    startActivity(new Intent(MainActivity.this,PantallaPrincipal.class));
+                    finish();
+                } else {
+                    System.out.println("error");
+                    //MainActivity.this.updateUI(null);
                 }
             }
         });
