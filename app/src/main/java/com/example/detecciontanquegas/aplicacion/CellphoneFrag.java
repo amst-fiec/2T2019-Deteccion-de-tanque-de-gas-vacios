533653fe65aea47f0e0c61abe56c87196c3a43e8
vicbrodr21 <vicbrodr@espol.edu.ec>
2020-01-24 02:36:09 -0500

    Zoom

--- app/src/main/java/com/example/detecciontanquegas/aplicacion/CellphoneFrag.java
+++ app/src/main/java/com/example/detecciontanquegas/aplicacion/CellphoneFrag.java
@@ -51,15 +51,15 @@ public class CellphoneFrag extends Fragment implements OnMapReadyCallback {
         /*MarkerOptions option = new MarkerOptions();
         option.position(ll).title("RESTAURANTE");
         map.moveCamera(CameraUpdateFactory.newLatLng(ll));*/
-        LatLng ll= new LatLng(-2.190945706245145,-79.88861690820404);
+        LatLng ll= new LatLng(-2.145794 ,-79.967028);
         map.addMarker(new MarkerOptions().position(ll).title("RESTAURANTE LAS DELICIAS"));
-        LatLng abestecedor1= new LatLng(-2.1910775435762133,-79.88823103746452);
+        LatLng abestecedor1= new LatLng(-2.146073,-79.966804);
         map.addMarker(new MarkerOptions().position(abestecedor1).title("TIENDA 1").snippet("SERVICIO A DOMICILIO LLAMAR AL 0956425847").icon(BitmapDescriptorFactory.fromResource(R.drawable.gas_cylinder2)));
-        LatLng abastecedor2= new LatLng(-2.1923009379551956,-79.8922997068783);
+        LatLng abastecedor2= new LatLng( -2.146884,-79.967174);
         map.addMarker(new MarkerOptions().position(abastecedor2).title("TIENDA 2").snippet("SERVICIO A DOMICILIO LLAMAR AL 0956452533").icon(BitmapDescriptorFactory.fromResource(R.drawable.gas_cylinder2)));
 
 
-        map.moveCamera(CameraUpdateFactory.newLatLng(ll));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(ll,50));
     }
 }
 
